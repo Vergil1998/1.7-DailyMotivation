d4fe297631267b3cf78427890f9e9e4dfadfa286
Daredevil <kaaan_s@yahoo.de>
2023-08-18 17:53:41 +0200

    - Chapter 13 of DailyMotivation

--- DailyMotivation/ContentView.swift
+++ DailyMotivation/ContentView.swift
@@ -8,9 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messageString = "Namaste"
+    @State private var messageString = ""
     @State private var imageName = ""
     @State private var imageNumber = 0
+    @State private var messages = ["You Are Fantastic!", "Your Code Journey Will Succeed!", "You Can Do It!", "Don't Give Up!", "It's Not Over If You Give Up!", "Keep Going!", "Don't Stop!", "Always Work!", "You Are A Champion!"]
+    @State private var messagesNumber = 0
     
     var body: some View {
         
@@ -36,11 +38,12 @@ struct ContentView: View {
             HStack {
                 Button("Show Message") {
                     
-                    let message1 = "You Are Awesome!"
-                    let message2 = "You Are Great!"
+                   messageString = messages[messagesNumber]
+                    messagesNumber += 1
+                    if messagesNumber == messages.count {
+                        messagesNumber = 0
+                    }
                     
-                    
-                    messageString = (messageString == message1 ? message2 : message1)
                     
                     //TODO: - update image number -
                     imageName = "image\(imageNumber)"
